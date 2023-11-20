6f29470a8adc142d50e97dd6328cb2065dffc463
Jerrell Yeoh <jerrellyeoh@gmail.com>
2023-11-20 19:38:21 +0800

    testing

--- CAM Application/src/UI/StudentUI.java
+++ CAM Application/src/UI/StudentUI.java
@@ -1,5 +1,7 @@
 package UI;
 
+import java.util.Scanner;
+
 public class StudentUI {
 
     //public void showWithdraw() {
@@ -7,8 +9,27 @@ public class StudentUI {
    // }
 	
     public void start() {
-    	//switch ()
-        //Switch case for all functions
+    	Scanner scan = new Scanner(System.in);    	
+    	int x = scan.nextInt();
+    	int y;
+    	
+    	switch (x) {
+    	case 1: //view available camps
+    		//visibility == true, faculty matches user's faculty
+    		break;
+    	
+    	case 2: //view registered camps
+    		//show list of camps, need a method to iterate thru camp database and search
+    		//array list of all the members in that camp
+    		y = scan.nextInt(); //choose a specific camp
+    	//	generateReport(); print camp info
+    		break;
+    	}
+    	
+    	case 3:
+    		break;
+    		
+    	//Switch case for all functions
         //1. View available camps
         //2. View registered camps
             // Show list of camps
