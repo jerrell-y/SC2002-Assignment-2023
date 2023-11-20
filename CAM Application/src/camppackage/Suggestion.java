6f97c2fa3561d80cf1748fc07a22c7718e49c4a7
dmwz1411 <150163478+dmwz1411@users.noreply.github.com>
2023-11-20 20:05:47 +0800

    Suggestion.java

--- CAM Application/src/camppackage/Suggestion.java
+++ CAM Application/src/camppackage/Suggestion.java
@@ -1,14 +1,19 @@
 package camppackage;
 
 public class Suggestion {
+	
     private String title;
-    private String content;
+    private String suggestion;
     private String campCommiteeName;
     private boolean status;
 
-    public Suggestion(String title, String content, String campCommiteeName) {
+    public Camp getCamp() {
+		return camp;
+	}
+    
+    public Suggestion(String title, String suggestion, String campCommiteeName) {
         this.title = title;
-        this.content = content;
+        this.suggestion = suggestion;
         this.campCommiteeName = campCommiteeName;
         this.status = false;
     }
@@ -16,4 +21,24 @@ public class Suggestion {
     public void setStatus(boolean status) {
         this.status = status;
     }
+    
+    public void viewSuggestion(String suggestion) {
+		System.out.println("Here is the suggestion submitted: " + suggestion);
+	}
+    
+    public String editSuggestion(String suggestion) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter the new suggestion: ");
+		String newSuggestion = sc.nextLine();
+		return newSuggestion;
+    }
+    
+    public String getSuggestion() {
+		return suggestion;
+	}
+    
+    public void setSuggestion(String suggestion) {
+		this.suggestion = suggestion;
+	}
+    
 }
