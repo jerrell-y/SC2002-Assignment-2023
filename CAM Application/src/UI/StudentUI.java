b57374a57cc323dfca7e2cb8a5b62afc85a8def4
Jerrell Yeoh <jerrellyeoh@gmail.com>
2023-11-23 22:33:58 +0800

    studentUI

--- CAM Application/src/UI/StudentUI.java
+++ CAM Application/src/UI/StudentUI.java
@@ -7,7 +7,6 @@ public class StudentUI {
     public void start() {
     	Scanner scan = new Scanner(System.in);    	
     	int x = scan.nextInt();
-    	int y;  //another scanner
 		System.out.println("Select Function");
         System.out.println("1. View available camps");
         System.out.println("2. View registered camps");
@@ -17,10 +16,46 @@ public class StudentUI {
     	switch (x) {
     	case 1:
 			viewManager.viewAvailableCamps();
-    		break;
+			System.out.println("Enter a campID: ");
+			x = scan.nextInt();
+			campDatabase.searchCampID(x);
+			
+			System.out.println("1. Register as committee");
+			System.out.println("2. Register as attendee");
+			System.out.println("3. Go back");
+			x = scan.nextInt();
+			switch(x){
+				case 1:
+					//get camp from the database with that ID
+					camp.registerCommittee();
+					break;
+						
+				case 2:
+					//get camp from the database with that ID
+					camp.registerAttendee();
+					break;
+
+				case 3:   //go back
+					break;
+			}
+
+    	break;
     	
     	case 2:
     		viewManager.viewRegisteredCamps();
+			System.out.println("1. Enter a campID: ");
+			System.out.println("2. Go back");
+			x = scan.nextInt();
+
+			switch(x){
+				case 1:
+
+					break;
+				case 2:  //go back
+					break;
+			}
+
+
     		break;
     	}
     	
