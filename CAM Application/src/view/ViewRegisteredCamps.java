119ce9428041fe63801487c6f933d6e358519fc7
Jerrell Yeoh <jerrellyeoh@gmail.com>
2023-11-24 18:55:04 +0800

    ui

--- CAM Application/src/view/ViewRegisteredCamps.java
+++ CAM Application/src/view/ViewRegisteredCamps.java
@@ -1,5 +1,11 @@
 package view;
 
 public class ViewRegisteredCamps implements ViewCamps {
-
+	public static void displayCamps(ArrayList<Camp> c) {
+        for (int i = 0; i < c.size(); i++) {
+            if (c.get(i).isCommittee || c.get(i).isAttendee){
+                System.out.println(c.get(i).getCampName() + c.get(i).getCampID());
+            }
+        }
+    }
 }
