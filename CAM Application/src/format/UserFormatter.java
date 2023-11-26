8fc920f3c9b5a695055ac4e33e079af96e2bfd44
blanknew <boyboystudies@gmail.com>
2023-11-26 22:40:52 +0800

    Added Javadoc for userFormatter

--- CAM Application/src/format/UserFormatter.java
+++ CAM Application/src/format/UserFormatter.java
@@ -6,8 +6,15 @@ import user.User;
  * Formats a user into a more readable String for printing purposes.
  */
 public class UserFormatter implements iFormatter<User>{
+    /**
+     * The instance of this class.
+     */
     private static UserFormatter instance;
     
+    /**
+     * Gets the instance of this class. If not created yet, create a new instance.
+     * @return this class' instance.
+     */
     public static synchronized UserFormatter getInstance() {
         if (instance == null) {
             instance = new UserFormatter();
@@ -15,6 +22,11 @@ public class UserFormatter implements iFormatter<User>{
         return instance;
     }
 
+    /**
+     * Formats a user into a long string containing all the information of the user.
+     * @param camp The user object.
+     * @return a String containing all information of the user.
+     */
     public String formatFull(User user) {
         return  "UserID = " + user.getUserID() +
                 "\nName = " + user.getName() + 
