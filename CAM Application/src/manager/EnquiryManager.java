a6c35985bc3a0f3dcded7a64b2a9ec6e15674fd0
Jerrell Yeoh <jerrellyeoh@gmail.com>
2023-11-26 21:58:13 +0800

    javadoc

--- CAM Application/src/manager/EnquiryManager.java
+++ CAM Application/src/manager/EnquiryManager.java
@@ -13,16 +13,31 @@ import user.UserManager;
  * The manager that handles the methods regarding enquiries.
  */
 public class EnquiryManager {
+    /**
+     * a variable that stores a specific enquiry into the manager
+     */
     private static Enquiry enquiry;
 
+    /**
+     * method that sets a specific enquiry into the manager
+     * @param index
+     */
     public static void setEnquiry(int index) {
         EnquiryManager.enquiry = CampManager.getCamp().getEnquiries().get(index);
     }
 
+    /**
+     * method that returns the specific enquiry stored in the manager
+     * @return the enquiry stored inside
+     */
     public static Enquiry getEnquiry() {
         return enquiry;
     }
 
+    /**
+     * method to add enquiry into a camp
+     * @param enq the string that will be added into the content of the camp
+     */
     public static void addEnquiry(String enq) { 
         User user = UserManager.getUser();
         Camp c = CampManager.getCamp();
@@ -31,6 +46,11 @@ public class EnquiryManager {
         CampDatabase.getInstance().update();
     }
 
+    /**
+     * method to edit the enquiry from the camp
+     * @param enq is the string that is used to overwrite the current content in that specific enquiry
+     * @return false if the enquiry has already been answered, and true if it has not yet been answered
+     */
     public static boolean editEnquiry(String enq) { 
         if (enquiry.isAnswered()) {
             System.out.println("The enquiry has already been answered!\n");
@@ -43,6 +63,12 @@ public class EnquiryManager {
         }
     }
 
+    /**
+     * method to delete enquiry from the camp
+     * @param enquiryNum gives us the index to remove the enquiry from the camp
+     * @return false if the enquiry has already been answered, and true if it has not
+     * 
+     */
     public static boolean deleteEnquiry(int enquiryNum) { 
         if (enquiry.isAnswered()) {
             System.out.println("The enquiry has already been answered!\n");
@@ -56,6 +82,12 @@ public class EnquiryManager {
         return true;
     }
 
+    /**
+     * method to reply the enquiry as a staff or a committee. If a committee replies, then they will recieve points
+     * @param reply is the string that is entered to reply to that enquiry
+     * @return false if the enquiry has already been answered, and true if it has not
+     * 
+     */
     public static boolean replyEnquiry(String reply) {          
         User user = UserManager.getUser();
         if (enquiry.isAnswered()) {
@@ -77,6 +109,11 @@ public class EnquiryManager {
     }
 
 
+    /**
+     * the method to print out all the current user's enquiries in that camp
+     * @return an ArrarList<Integer> of all the indexes of the current list being looked at, to be used later 
+     * 
+     */
     public static ArrayList<Integer> printUserEnquiry() { 
         Camp c = CampManager.getCamp();
         User user = UserManager.getUser();
@@ -99,7 +136,11 @@ public class EnquiryManager {
         return userEnquiries; 
     }
 
-    //returns an ArrarList<Integer> of all the indexes of the current list being looked at, to be used later 
+    /**
+     * method to print out all the enquiries in that camp
+     * @return an ArrarList<Integer> of all the indexes of the current list being looked at, to be used later 
+     * 
+     */
     public static ArrayList<Integer> printAllEnquiry() { 
         Camp c = CampManager.getCamp();
         ArrayList<Enquiry> eqr = c.getEnquiries();
