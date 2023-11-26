b0a299e9c692cfe7f755151fc5f85d83d60a2055
Kenneth <44108312+wjkenneth@users.noreply.github.com>
2023-11-26 23:19:14 +0800

    login javadocs

--- CAM Application/src/login/PasswordManager.java
+++ CAM Application/src/login/PasswordManager.java
@@ -3,9 +3,17 @@ package login;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 /**
- * The manager that handles the methods regarding passwords.
+ * The PasswordManager class provides functionalities related to password encryption and verification.
+ * It uses SHA-256 hashing algorithm to secure passwords.
  */
 public class PasswordManager {
+    /**
+     * Encrypts a plaintext password using SHA-256 hashing algorithm.
+     *
+     * @param password The plaintext password to be encrypted.
+     * @return A string representing the hexadecimal format of the hashed password.
+     * @throws RuntimeException If the SHA-256 hashing algorithm is not available.
+     */
     public static String encrypt(String password) {
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
@@ -16,11 +24,25 @@ public class PasswordManager {
         }
     }
 
+    /**
+     * Compares an encrypted password with a plaintext password after hashing it.
+     * This method is typically used to verify user login attempts.
+     *
+     * @param encryptedPassword The already encrypted password to compare against.
+     * @param password The plaintext password to check.
+     * @return true if the plaintext password matches the encrypted password, false otherwise.
+     */
     public static boolean checkPassword(String encryptedPassword, String password) {
         String hashedPassword =  encrypt(password);
         return hashedPassword.equals(encryptedPassword);
     }
 
+    /**
+     * Converts a byte array from the hashed password into a hexadecimal string.
+     *
+     * @param hash The byte array output from the hashing algorithm.
+     * @return A string representing the hexadecimal values of the hash.
+     */
     private static String bytesToHex(byte[] hash) {
         StringBuilder hexString = new StringBuilder(2 * hash.length);
         for (int i = 0; i < hash.length; i++) {
